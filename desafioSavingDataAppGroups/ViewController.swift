df98a2e3a107ce2f963dd8c309c6a65328527b94
Ítalo Chesley <igomes@ciandt.com>
2018-02-18 11:39:09 -0300

    implementando solução

--- desafioSavingDataAppGroups/ViewController.swift
+++ desafioSavingDataAppGroups/ViewController.swift
@@ -10,21 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let keyDesafioSavingData = "group.desafioSavingData"
+    let keyParametroNumeroVisitas = "parametroNumeroVisitas"
+    
     @IBOutlet weak var numeroVisitasLabel: UILabel!
+    @IBOutlet weak var numeroVisitarStepper: UIStepper!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        numeroVisitarStepper.value = obterNumeroVisitas()
+        setarLabelComStepper()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func incrementarNumeroVisitas(_ sender: UIStepper) {
-        numeroVisitasLabel.text = String(sender.value)
+        ajustarNumeroVisitas(numeroVisitasRequeridas: sender.value)
+        setarLabelComStepper()
     }
     
+    func setarLabelComStepper() {
+        numeroVisitasLabel.text = String(numeroVisitarStepper.value)
+    }
     
+    func obterNumeroVisitas() -> Double {
+        let userDefaults = UserDefaults(suiteName: keyDesafioSavingData)
+        return userDefaults?.double(forKey: keyParametroNumeroVisitas) ?? 1
+    }
+    
+    func ajustarNumeroVisitas(numeroVisitasRequeridas: Double) {
+        let userDefaults = UserDefaults(suiteName: keyDesafioSavingData)
+        userDefaults?.set(numeroVisitasRequeridas, forKey: keyParametroNumeroVisitas)
+    }
 }
 
