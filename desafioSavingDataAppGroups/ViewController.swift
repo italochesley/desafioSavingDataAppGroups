78f1e4a820af9615a4c68b10c665e4bf0e946bd5
Ítalo Chesley <igomes@ciandt.com>
2018-02-18 11:23:01 -0300

    incluindo arquivos

--- desafioSavingDataAppGroups/ViewController.swift
+++ desafioSavingDataAppGroups/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numeroVisitasLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func incrementarNumeroVisitas(_ sender: UIStepper) {
+        numeroVisitasLabel.text = String(sender.value)
+    }
+    
+    
 }
 
